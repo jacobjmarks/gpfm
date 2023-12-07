6409262bc774451b18a60a7be3efcc14e171adcf
Jacob Marks <jacobjmarks@outlook.com>
2023-12-07 12:58:17 +1000

    Configure row numbers via Converter

---
+++ Gpfm.Gui/Converters/StepNumberConverter.cs
@@ -0,0 +1,25 @@
+﻿using Gpfm.Core;
+using System.Globalization;
+
+namespace Gpfm.Gui.Converters;
+
+public class StepNumberConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not JobStep step)
+            return null;
+
+        var indexOf = MainPage.Steps.IndexOf(step);
+
+        if (indexOf < 0)
+            throw new InvalidOperationException();
+
+        return indexOf + 1;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
